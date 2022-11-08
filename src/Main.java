14373790e96f63773bab4c07e1fed5c35477ff7f
cDolz <carlosdolzm@gmail.com>
2022-11-08 14:27:55 +0100

    master

--- src/Main.java
+++ src/Main.java
@@ -1,87 +1,99 @@
-import javax.swing.*;
-
 public class Main {
-    public static int bingo[][] = new int[3][9];
-    public static int k = 0;
+    public static int[][] bingo = new int[3][9];
+    public static String[][] sBingo = new String[3][9];
+    public static int columna;
+    public static int numRandom;
+    public static boolean flag = false;
 
     public static void main(String[] args) {
-        boolean flag;
-        int rangoUno = 0;
-        int rangoDos = 10;
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
                 do {
-                    try {
-                        bingo[j][k] = Integer.parseInt(JOptionPane.showInputDialog("Dime un numero entre el " +
-                                rangoUno + " y el " + rangoDos));
-                        if (bingo[j][k] != 0 && (bingo[j][k] >= rangoUno && bingo[j][k] < rangoDos)) {
-                            flag = true;
-                            if (bingo[1][k] != 0 && (bingo[1][k] == bingo[0][k] ||
-                                    bingo[1][k] == bingo[2][k])) {
-                                flag = false;
-                                JOptionPane.showMessageDialog(null, "Valor incorrecto");
-                            } else if (bingo[2][k] != 0 && (bingo[2][k] == bingo[0][k] ||
-                                    bingo[2][k] == bingo[1][k])) {
-                                flag = false;
-                                JOptionPane.showMessageDialog(null, "Valor incorrecto");
-                            }
-                        } else {
-                            JOptionPane.showMessageDialog(null, "Valor incorrecto");
-                            flag = false;
-                        }
-                    } catch (Exception e) {
-                        JOptionPane.showMessageDialog(null, "Valor incorrecto");
-                        flag = false;
-                    }
+                    generateRandom();
+                    setValue(i);
                 } while (!flag);
             }
-            ordenar();
-            rangoUno += 10;
-            rangoDos += 10;
-            k++;
         }
+        bingoToString();
+        printSBingo();
+        generateRandomPlayNums();
+    }
+
+    private static void generateRandomPlayNums() {
+        int[] allNums = new int[90];
+        int contador = 0;
+        do {
+            numRandom = (int) (Math.random() * (90 - 1) + 1);
+            if (comprobarPlayNum(allNums)) {
+                allNums[contador] = numRandom;
+                contador++;
+            }
+        }while (contador < 90);
+    }
+
+    private static boolean comprobarPlayNum(int[] allNums) {
+        int contador = 0;
+        flag = true;
+        for (int i = 0; i < allNums.length; i++) {
+            if (numRandom == allNums[i]) {
+                contador++;
+            }
+        }
+        if (contador != 0){
+            flag = false;
+        }
+        return flag;
+    }
+
+    private static void bingoToString() {
         for (int i = 0; i < bingo.length; i++) {
             for (int j = 0; j < bingo[i].length; j++) {
-                System.out.print(bingo[i][j] + ", ");
+                sBingo[i][j] = String.valueOf(bingo[i][j]);
             }
-            System.out.println("");
         }
     }
 
-    public static void ordenar() {
-        if ( bingo[0][k] >  bingo[1][k] &&  bingo[0][k] >  bingo[2][k]) {
-            int aux = 0;
-            if (bingo[1][k] > bingo[2][k]) {
-                aux = bingo[0][k];
-                bingo[0][k] = bingo[2][k];
-                bingo[2][k] = aux;
-            } else {
-                aux =  bingo[0][k];
-                bingo[0][k] = bingo[1][k];
-                bingo[1][k] = bingo[2][k];
-                bingo[2][k] = aux;
-            }
-        } else if (bingo[1][k] > bingo[0][k] && bingo[1][k] > bingo[2][k]) {
-            int aux = 0;
-            int auxDos = 0;
-            if ( bingo[0][k] > bingo[2][k]) {
-                aux = bingo[0][k];
-                auxDos = bingo[1][k];
-                bingo[0][k] = bingo[2][k];
-                bingo[1][k] = aux;
-                bingo[2][k] = auxDos;
-            } else {
-                aux = bingo[1][k];
-                bingo[1][k] = bingo[2][k];
-                bingo[2][k] = aux;
-            }
+    private static void setValue(int i) {
+        if (bingo[i][columna] == 0 && notSup() && numRandom != 0) {
+            bingo[i][columna] = numRandom;
+            flag = true;
         } else {
-            if ( bingo[0][k] >  bingo[1][k]) {
-                int aux = bingo[0][k];
-                bingo[0][k] = bingo[1][k];
-                bingo[1][k] = aux;
-            }
+            flag = false;
         }
+    }
 
+    private static boolean notSup() {
+        if (bingo[0][columna] != 0 && numRandom <= bingo[0][columna]) {
+            flag = false;
+        } else if (bingo[0][columna] == 0 && bingo[1][columna] != 0 && numRandom <= bingo[1][columna]) {
+            flag = false;
+        } else flag = bingo[0][columna] == 0 || bingo[1][columna] == 0;
+        return flag;
+    }
+
+    private static void generateRandom() {
+        columna = (int) (Math.random() * 9);
+        int min = columna * 10;
+        int max = (columna * 10) + 9;
+        if (columna == 9) {
+            max = 90;
+        }
+        numRandom = (int) (Math.random() * (max - min) + min);
+    }
+
+    private static void printSBingo() {
+        for (int i = 0; i < sBingo.length; i++) {
+            for (int j = 0; j < sBingo[i].length; j++) {
+                if (sBingo[i][j].equals("0")) {
+                    sBingo[i][j] = " ";
+                }
+                if (sBingo[i][j].length() == 1) {
+                    System.out.print("[  " + sBingo[i][j] + " ]");
+                } else {
+                    System.out.print("[ " + sBingo[i][j] + " ]");
+                }
+            }
+            System.out.println();
+        }
     }
 }
