3a4aafde872d4e635e5dbe79006be11bfd49df43
cDolz <carlosdolzm@gmail.com>
2022-11-10 11:03:36 +0100

    master

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import javax.swing.*;
+
 public class Main {
     public static int[][] bingo = new int[3][9];
     public static String[][] sBingo = new String[3][9];
     public static int columna;
     public static int numRandom;
     public static boolean flag = false;
+    public static int contadorLinea;
+    public static int contadorBingo;
 
     public static void main(String[] args) {
         for (int i = 0; i < 3; i++) {
@@ -16,30 +20,80 @@ public class Main {
         }
         bingoToString();
         printSBingo();
+        System.out.println();
         generateRandomPlayNums();
     }
 
     private static void generateRandomPlayNums() {
         int[] allNums = new int[90];
-        int contador = 0;
+        int contadorNums = 0;
         do {
             numRandom = (int) (Math.random() * (90 - 1) + 1);
-            if (comprobarPlayNum(allNums)) {
-                allNums[contador] = numRandom;
-                contador++;
+            if (validarPlayNum(allNums)) {
+                allNums[contadorNums] = numRandom;
+                contadorNums++;
+                JOptionPane.showMessageDialog(null, "El " + numRandom + "!");
             }
-        }while (contador < 90);
+            comprobarPlayNum();
+            printSBingo();
+            if (contadorLinea < 1) {
+                comprobarLinea();
+            }
+            comprobarBingo();
+            System.out.println();
+        } while (contadorBingo < 1);
     }
 
-    private static boolean comprobarPlayNum(int[] allNums) {
-        int contador = 0;
-        flag = true;
-        for (int i = 0; i < allNums.length; i++) {
-            if (numRandom == allNums[i]) {
-                contador++;
+    private static void comprobarLinea() {
+        for (String[] strings : sBingo) {
+            int contador = 0;
+            for (String string : strings) {
+                if (string.equals("X") || string.equals(" ")) {
+                    contador++;
+                }
+                if (contador == 9) {
+                    contadorLinea++;
+                    JOptionPane.showMessageDialog(null, "LINEA!");
+                }
             }
         }
-        if (contador != 0){
+    }
+
+    private static void comprobarBingo() {
+        int contador = 0;
+        for (String[] strings : sBingo) {
+            for (String string : strings) {
+                if (string.equals("X") || string.equals(" ")) {
+                    contador++;
+                }
+                if (contador == 27) {
+                    contadorBingo++;
+                    JOptionPane.showMessageDialog(null, "¡¡BINGO!!");
+                }
+            }
+        }
+    }
+
+    private static void comprobarPlayNum() {
+        for (int i = 0; i < sBingo.length; i++) {
+            for (int j = 0; j < sBingo[i].length; j++) {
+                if (sBingo[i][j].equals(Integer.toString(numRandom))) {
+                    sBingo[i][j] = "X";
+                }
+            }
+        }
+    }
+
+    private static boolean validarPlayNum(int[] allNums) {
+        int contador = 0;
+        flag = true;
+        for (int allNum : allNums) {
+            if (numRandom == allNum) {
+                contador++;
+                
+            }
+        }
+        if (contador != 0) {
             flag = false;
         }
         return flag;
